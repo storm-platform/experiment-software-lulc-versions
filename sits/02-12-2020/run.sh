5f9bb7192769ace4b17de12cd11521ed87315d21
Phelps <phelpsmemo@gmail.com>
2021-04-14 09:21:25 -0300

    update workdir and entrypoint command

--- sits/02-12-2020/run.sh
+++ sits/02-12-2020/run.sh
@@ -8,10 +8,13 @@ SITS_COMMIT_HASH_VERSION=13f6867b9e6b5f5e226ad5d97b93267fa8267d75
 cd environment
 ./build.sh
 
+cd ..
+
 #
 # run
 #
 docker run -ti -d --name sits_classification_${SITS_COMMIT_HASH_VERSION} \
     -v ${PWD}/classification.R:/opt/classification/classification_script.R \
     -v ${PWD}/classification-results:/opt/classification/results \
-    m3nin0/rep-lulc-version:${SITS_COMMIT_HASH_VERSION}
+    m3nin0/rep-lulc-version:${SITS_COMMIT_HASH_VERSION} \
+    Rscript classification_script.R
--- sits/30-06-2020/run.sh
+++ sits/30-06-2020/run.sh
@@ -8,10 +8,13 @@ SITS_COMMIT_HASH_VERSION=f0a9de020c2c4d2891523cd14ca452aac69790ae
 cd environment
 ./build.sh
 
+cd ..
+
 #
 # run
 #
 docker run -ti -d --name sits_classification_${SITS_COMMIT_HASH_VERSION} \
     -v ${PWD}/classification.R:/opt/classification/classification_script.R \
     -v ${PWD}/classification-results:/opt/classification/results \
-    m3nin0/rep-lulc-version:${SITS_COMMIT_HASH_VERSION}
+    m3nin0/rep-lulc-version:${SITS_COMMIT_HASH_VERSION} \
+    Rscript classification_script.R
