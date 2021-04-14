4c9bfc0132dab71d18234b2fb488d9752612a84e
Phelps <phelpsmemo@gmail.com>
2021-04-14 01:34:16 -0300

    add build to execution

--- sits/02-12-2020/run.sh
+++ sits/02-12-2020/run.sh
@@ -2,6 +2,17 @@
 
 SITS_COMMIT_HASH_VERSION=13f6867b9e6b5f5e226ad5d97b93267fa8267d75
 
+#
+# build
+#
+cd environment
+docker build -t "m3nin0/rep-lulc-version:${SITS_COMMIT_HASH_VERSION}" .
+
+cd ..
+
+#
+# run
+#
 docker run -ti -d --name sits_classification_${SITS_COMMIT_HASH_VERSION} \
     -v ${PWD}/classification.R:/opt/classification/classification_script.R \
     -v ${PWD}/classification-results:/opt/classification/results \
--- sits/30-06-2020/run.sh
+++ sits/30-06-2020/run.sh
@@ -2,6 +2,17 @@
 
 SITS_COMMIT_HASH_VERSION=f0a9de020c2c4d2891523cd14ca452aac69790ae
 
+#
+# build
+#
+cd environment
+docker build -t "m3nin0/rep-lulc-version:${SITS_COMMIT_HASH_VERSION}" .
+
+cd ..
+
+#
+# run
+#
 docker run -ti -d --name sits_classification_${SITS_COMMIT_HASH_VERSION} \
     -v ${PWD}/classification.R:/opt/classification/classification_script.R \
     -v ${PWD}/classification-results:/opt/classification/results \
